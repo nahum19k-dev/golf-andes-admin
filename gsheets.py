b952864d87b10f0231d3e6700e6f30330d8195e2
nahum19k-dev <nahum19k@gmail.com>
2026-03-22 22:45:38 -0500

    Add Google Sheets integration with Streamlit

--- gsheets.py
+++ gsheets.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+@st.cache_resource
+def get_sheet(nombre_hoja):
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=SCOPES
+    )
+    client = gspread.authorize(creds)
+    sheet_id = st.secrets["sheets"]["spreadsheet_id"]
+    spreadsheet = client.open_by_key(sheet_id)
+    return spreadsheet.worksheet(nombre_hoja)
